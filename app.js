c21f23fd7a4fab96c47d7efadd9be4cd538d1474
Dyota Tanuwibawa <dyota257@gmail.com>
2020-07-15 00:04:06 +0800

    Implement one backspace

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
+// document.addEventListener("keydown", function() {
+//     document.querySelector(".writepage").innerHTML = document.querySelector(".writepage").innerHTML + String.fromCharCode(33 + Math.floor(Math.random() * (126 - 33)));
+//   })
+
+
+
 document.addEventListener("keydown", function() {
-    document.querySelector("body").innerHTML = document.querySelector("body").innerHTML + String.fromCharCode(33 + Math.floor(Math.random() * (126 - 33)));
-  })
+  var key = event.key;
+  var text = document.querySelector(".writepage");
+  switch (key) {
+    
+    case "Backspace":
+      // if (text.innerHTML.trim().lastIndexOf("<span")===-1){
+        var lastSpace = text.innerHTML.trim().lastIndexOf(" ");
+        var textHead = text.innerHTML.slice(0, lastSpace);
+        console.log(textHead);
+        var textTail = text.innerHTML.slice(lastSpace);
+        console.log(textTail);
+        text.innerHTML = textHead+ "<span style=\"text-decoration:line-through double; color:darkred\">" + textTail + "</span>";
+      // } 
+      // else {
+      //   var lastSpan = text.innerHTML.trim().lastIndexOf("<span");
+      //   var textHead = text.textContent.slice(0, lastSpan);
+      //   var lastSpace = textHead.trim().lastIndexOf(" ");
+      //   var textHeadHead = textHead.slice(0, lastSpace);
+
+      //   var textHeadTail = textHead.slice(lastSpace);
+        
+      //   textHead = textHeadHead + "<span style=\"text-decoration:line-through double; color:darkred\">" + textHeadTail + "</span>";
+        
+      //   var textTail = text.innerHTML.slice(lastSpan);
+        
+      //   text.innerHTML = textHead + textTail;
+      //   console.log(textHead);
+      // }
+
+      console.log("last space at " + text.innerHTML.lastIndexOf(" "))
+      break;
+    case "Shift":
+      break;
+    case "CapsLock":
+      break;
+    case "Control":
+      break;
+    case "Alt":
+      break;
+    case "Meta":
+      break;
+    case "Tab":
+      text.innerHTML = text.innerHTML + "    ";
+      break;
+    case "Enter":
+      text.innerHTML = text.innerHTML + "<br>";
+      break;
+    default:
+      text.innerHTML = text.innerHTML + event.key;
+      break;
+  }
+
+  console.log(text.innerHTML);
+  console.log(event.key);
+})
